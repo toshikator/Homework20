c37e1152156f5e0f46940b8617a02baa7e7832bf
Anton Bukhman <diamondspb2015@gmail.com>
2023-01-13 00:28:48 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+(()=>{
+    const imgDB = [
+        './img/1.jpg',
+        './img/2.jpg',
+        './img/3.jpg',
+        './img/4.jpg',
+        './img/5.jpg',
+        './img/6.jpg',
+        './img/7.jpg',
+        './img/8.jpg',
+        './img/9.jpg',
+        './img/10.jpg',
+        './img/11.jpg',
+    ];
+
+    const render = () =>{
+        //TODO add to #imgList images 80*80
+        document.createElement('img');//variant 1
+        someElement.innerHTML = imgDB.map(()=>{//variant 2
+
+        }).join('');
+
+        //TODO add to #imgList 'click' eventListener
+    }
+
+
+
+
+
+
+
+
+
+
+
+})()
