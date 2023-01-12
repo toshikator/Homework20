b5b53c2e1ca2efc46fc0c82e4d7ea5ca0f74bed4
Anton Bukhman <diamondspb2015@gmail.com>
2023-01-13 01:38:25 +0200

    start to build

--- js/script.js
+++ js/script.js
@@ -15,22 +15,30 @@
 
     const render = () =>{
         //TODO add to #imgList images 80*80
-        document.createElement('img');//variant 1
-        someElement.innerHTML = imgDB.map(()=>{//variant 2
+        imgDB.forEach((element)=> {
+            newImg = document.createElement('img');//variant 1
+            newImg.src = element;
+            newImg.classList.add('imgContent');
+            document.querySelector('#imgList').appendChild(newImg);
+        });
 
-        }).join('');
+
+        // someElement.innerHTML = imgDB.map(()=>{//variant 2
+        // }).join('');
 
         //TODO add to #imgList 'click' eventListener
     }
 
+    console.log(imgDB);
+
+    document.querySelector('#imgList').addEventListener('click',(event)=>{
+
+    });
 
 
 
 
 
 
-
-
-
-
+    render();
 })()
